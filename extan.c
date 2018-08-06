300aa21513e06fd7940ea7fab55f8e9a2d8494b5
Chandlerjoe64 <chandlerjoe64@gmail.com>
2018-08-06 17:56:54 -0500

    test C file

---
+++ extan.c
@@ -0,0 +1,17 @@
+/*
+Joseph A. Chandler
+email: chandlerjoe64@gmail.com
+*/
+
+/*
+EXTAN (tEXT_ANanlysis) is a program which iterates over a given body of text to derive repeated patters of words. Its purpose is to identify phrases or other 
+repeating patterns to aid in the creation of password dictionaries.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char*argv[]) {
+	printf("Hello, world!\n");
+
+}
